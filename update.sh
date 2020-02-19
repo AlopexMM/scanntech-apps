74ddbbfde5d26161b6eb6bf3b8650329184a39c9
AlopexMM <mariomori00@gmail.com>
2020-02-19 10:47:59 -0300

    se cambiaron requerimientos para el install y se suma el script de update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd $HOME/.scanntech-apps/
+
+git clone https://github.com/AlopexMM/scanntech-apps.git
+
+cp -r $HOME/.scanntech-apps/scanntech-apps/module_cli $HOME/.scanntech-apps/
+
+rm -rf $HOME/.scanntech-apps/scanntech-apps
