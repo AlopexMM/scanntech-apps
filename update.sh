3502dc91e744710bdcfadd65d9cc72738b1bbad9
AlopexMM <mariomori00@gmail.com>
2020-05-21 16:38:49 -0300

    Se corrigio update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ cd $HOME/.scanntech-apps/
 
 git clone https://github.com/AlopexMM/scanntech-apps.git
 
-cp -r $HOME/.scanntech-apps/scanntech-apps/module_cli $HOME/.scanntech-apps/
+cp -r $HOME/.scanntech-apps/scanntech-apps/module_cli/* $HOME/.scanntech-apps/
 
-rm -rf $HOME/.scanntech-apps/scanntech-apps
+rm -rf $HOME/scanntech-apps
