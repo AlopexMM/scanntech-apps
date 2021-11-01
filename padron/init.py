856669a6924d1994fb6ad4daa9ecd5e5e64f1450
AlopexMM <mmori@alopexmm.ar>
2021-11-01 11:18:41 -0300

    Fix in padron

--- padron/init.py
+++ padron/init.py
@@ -18,18 +18,18 @@ class Padron(object):
         return
 
     def run(self):
-        for file in self.argv[1:]:
+        for file in self.argv:
             print(f"Procesando {file}")
             with open(file,mode="r",encoding="latin-1") as f:
                 for line in f.readlines():
                     line = line.replace("\n","")
                     if len(line) == 52:
                         l = line.split(";")
-                        lf = f"P;{l[1]};{l[2]};{l[3]};{l[4]};{l[5]};{l[6]};{l[7]};{l[8]};;1\n"
+                        lf = f"P;{l[1]};{l[2]};{l[3]};{l[4]};{l[5]};{l[6]};{l[7]};{l[8]};;6\n"
                         self._export(lf)
                     if len(line) == 55:
                         l = line.split(";")
-                        lf = f"P;{l[1]};{l[2]};{l[3]};{l[4]};{l[5]};{l[6]};{l[7]};{l[8]};{l[9]};6\n"
+                        lf = f"P;{l[1]};{l[2]};{l[3]};{l[4]};{l[5]};{l[6]};{l[7]};{l[8]};{l[9]};1\n"
                         self._export(lf)
         print("Se procesaron los padrones se va a exportar el reporte final")
         print(f"Se gravo el archivo {self.file}")
